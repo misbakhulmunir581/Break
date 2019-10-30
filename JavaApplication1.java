725507c54af8a5b6446362d95316ab27e1d76c3b
misbakhulmunir581 <56871810+misbakhulmunir581@users.noreply.github.com>
2019-10-30 20:47:40 +0700

    Add files via upload

---
+++ JavaApplication1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author user
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for (int nilai = 90; nilai > 80; nilai --) {
+      if (nilai == 83) {
+          break;
+      } 
+       System.out.println(nilai) ;
+        }
+        }
+
+
+}
